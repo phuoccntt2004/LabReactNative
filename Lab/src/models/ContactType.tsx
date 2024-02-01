350c2419c02ad882ea1d26aaa049ca6bf858b1e4
phuocdz5 <122171813+phuocdz5@users.noreply.github.com>
2024-02-01 18:23:40 +0700

    Delete Lab directory

--- Lab/src/models/ContactType.tsx
+++
@@ -1,9 +0,0 @@
-import { ImageSourcePropType } from "react-native"
-
-export interface ContactType{
-    dulieu:{
-        name:string,
-        position:string,
-        photo:ImageSourcePropType
-    }
-}
